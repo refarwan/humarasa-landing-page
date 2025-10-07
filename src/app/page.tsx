5bcd3b6c9aa5e4b4b0421b71866a14a2c4e8c77a
Aan Refarwan <refarwan@gmail.com>
2025-10-08 05:50:09 +0700

    feat: add hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <></>;
+    return (
+        <main className="pt-[73px]">
+            <div className="flex h-[407px] w-full flex-col items-center justify-center gap-[20px] bg-gray-400/75 bg-[url('/hero.png')] bg-cover bg-center bg-no-repeat px-[29px] text-white bg-blend-multiply">
+                <h2 className="max-w-[500px] text-center text-[28px] font-bold">
+                    Berdaya Bersama, Berkarya untuk Sesama
+                </h2>
+                <p className="max-w-[500px] text-center">
+                    Dedikasi kami untuk membangun masyarakat yang kuat,
+                    inklusif, dan berkelanjutan.
+                </p>
+                <Link
+                    href={"#"}
+                    className="mx-auto block h-[34px] w-max rounded-[4px] border-[2px] border-white px-[24px] text-center"
+                >
+                    OUR MISSION
+                </Link>
+            </div>
+        </main>
+    );
 }
